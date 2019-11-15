6ab4b3db200dfcca3cea9bb10abe9036cd17e206
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2019-11-15 16:52:18 -0200

    Atributos, getters, setters e construtor da calsse Desenvolvedor adicionados

--- LojaJogos/src/desenvolvedores/Desenvolvedor.java
+++ LojaJogos/src/desenvolvedores/Desenvolvedor.java
@@ -1,5 +1,23 @@
 package desenvolvedores;
 
-public class Desenvolvedor {
+import usuarios.Usuario;
 
+public class Desenvolvedor extends Usuario
+{
+	
+	private double porcentagemVenda;
+	
+	public Desenvolvedor(String nome,String numero,double porcentagemVenda)
+	{
+		super(nome,numero);
+		this.porcentagemVenda=this.porcentagemVenda;
+	}
+	public double getPorcentagemVenda() {
+		return porcentagemVenda;
+	}
+
+	public void setPorcentagemVenda(double porcentagemVenda) {
+		this.porcentagemVenda = porcentagemVenda;
+	}
+	
 }
