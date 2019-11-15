fa1fc7dd489b6211a6558d55deb4b161343e7dce
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2019-11-15 16:52:04 -0200

    Atributos, getters, setters e construtor da calsse Pessoa Juridica adicionados

--- LojaJogos/src/usuarios/PessoaJuridica.java
+++ LojaJogos/src/usuarios/PessoaJuridica.java
@@ -1,5 +1,19 @@
 package usuarios;
 
 public class PessoaJuridica extends Usuario {
+	private String cnpj;
 
+	public PessoaJuridica(String nome,String email,String cnpj)
+	{
+		super(nome,email);
+		this.cnpj=cnpj;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
 }
