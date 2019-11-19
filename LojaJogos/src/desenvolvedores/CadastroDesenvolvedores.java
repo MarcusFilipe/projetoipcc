ccf94a68b751e62ba98adb0af1037e9c82d24075
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2019-11-19 10:12:27 -0200

    CadastroDesenvolvedores: caso de possivel inserção e construtor inseridos

--- LojaJogos/src/desenvolvedores/CadastroDesenvolvedores.java
+++ LojaJogos/src/desenvolvedores/CadastroDesenvolvedores.java
@@ -2,6 +2,16 @@ package desenvolvedores;
 
 public class CadastroDesenvolvedores 
 {
-	
+	private RepositorioDesenvolvedores desenvolvedores;
+	public CadastroDesenvolvedores(RepositorioDesenvolvedores desenvolvedores)
+	{
+		this.desenvolvedores=desenvolvedores;
+	}
+	public void cadastrar(Desenvolvedor desenvolvedor)
+	{
+		if(!desenvolvedores.existe(desenvolvedor.getNome()))
+			desenvolvedores.inserir(desenvolvedor);
+		//else exception desenvolvedorjacadastrado
+	}
 
 }
