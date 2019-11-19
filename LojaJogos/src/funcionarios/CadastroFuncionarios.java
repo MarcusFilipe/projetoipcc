3c06c09927e1518d9f41f21a16ff91f3fd68982b
pvlb <pvlb@G3C25.windows.cin.ufpe.br>
2019-11-19 15:46:09 -0300

    Cadastro (Regra-Negocios) finalizada

--- LojaJogos/src/funcionarios/CadastroFuncionarios.java
+++ LojaJogos/src/funcionarios/CadastroFuncionarios.java
@@ -4,18 +4,21 @@ public class CadastroFuncionarios {
 	private RepositorioFuncionario funcionario;
 
 	CadastroFuncionarios(RepositorioFuncionario repositorio) {
-		funcionario = repositorio;
+		this.funcionario = repositorio;
 	}
 
 	public void cadastrar(Funcionario dados) throws FuncionarioJaCadastradoException {
-		if (funcionario.existe(dados.getCarteiraTrabalho())) {
+		if (this.funcionario.existe(dados.getCarteiraTrabalho())) {
 			throw new FuncionarioJaCadastradoException(dados);
 		} else
 			funcionario.inserir(dados);
 	}
 	
-	public void procurar(String carteiraTrabalho) {
-		
+	public Funcionario procurar(String carteiraTrabalho) throws FuncionarioNaoCadastradoException {
+		if (this.funcionario.existe(carteiraTrabalho)) {
+			return funcionario.procurar(carteiraTrabalho);
+		}else
+			throw new FuncionarioNaoCadastradoException();
 	}
 
 	public void remover(Funcionario dados) throws FuncionarioNaoCadastradoException {
