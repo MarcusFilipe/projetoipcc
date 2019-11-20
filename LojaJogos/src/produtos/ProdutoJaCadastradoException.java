030ee44b69465033eb0001c7bacf59456ad1cdcc
PC <PC@192.168.15.11>
2019-11-20 00:47:34 -0200

    produto quase pronto ALL

--- LojaJogos/src/produtos/ProdutoJaCadastradoException.java
+++ LojaJogos/src/produtos/ProdutoJaCadastradoException.java
@@ -4,5 +4,5 @@ public class ProdutoJaCadastradoException extends Exception {
 	public ProdutoJaCadastradoException(Produto dados) {
 		super("Produto " + dados.getNome() + " de ID " + dados.getId() + " ja cadastrado");
 	}
-	
+	//q
 }
