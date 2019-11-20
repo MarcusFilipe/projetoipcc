6a3c527a91189331286391de36fb230e49654dda
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2019-11-20 02:21:18 -0200

    Interface com a classe Desenvolvedor adicionada a main e com o try catch com os exceptions da classe Desenvolvedor

--- LojaJogos/src/ui/Programa.java
+++ LojaJogos/src/ui/Programa.java
@@ -7,7 +7,7 @@ import produtos.*;
 import usuarios.*;
 import desenvolvedores.*;
 import funcionarios.*;
-
+import java.util.*;
 public class Programa {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -18,81 +18,99 @@ public class Programa {
 		Cliente cl3 = new Cliente("Pedro","pvlb","105.930.450-89","ÈOuzi");
 		Cliente cl4 = new Cliente("Frederico","fbsm","193.149.560-69","Fred");
 		
+		
+		
 		RepositorioClientes arrayClientes = new RepositorioClienteArray(100);
 		RepositorioClientes listaClientes = new RepositorioClienteLista();
 		RepositorioDesenvolvedores arrayDesenvolvedores = new RepositorioDesenvolvedorArray(100);
 		RepositorioDesenvolvedores listaDesenvolvedores = new RepositorioDesenvolvedorLista();
 		RepositorioFuncionario arrayFuncionarios = new RepositorioFuncionarioArray(100);
 		RepositorioFuncionario listaFuncionarios = new RepositorioFuncionarioLista();
-		
-		
+
 		Fachada f = new Fachada(arrayClientes, arrayDesenvolvedores, arrayFuncionarios);
 		Fachada f1 = new Fachada(listaClientes, listaDesenvolvedores, listaFuncionarios);
-		//array
 		
-		
-		try {
-			f.cadastrarCliente(cl);
-			f.cadastrarCliente(cl1);
-			f.existeCliente("046.468.694-62");
-		}catch(ClienteNaoCadastradoException e) {
-			System.out.println(e.getMessage());
-		}catch(ClienteJaCadastradoException e1) {
-			System.out.println(e1.getMessage());
-		}
-		
-		arrayClientes.inserir(cl1);
-		System.out.println(arrayClientes.existe("192.949.340-53"));
-		System.out.println(arrayClientes.existe("189.340.503-12"));
-		
-		try{
-			arrayClientes.remover("192.949.340-53");
-			arrayClientes.remover("123");
-		}catch(ClienteNaoCadastradoException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		try{
-			Cliente c = arrayClientes.procurar("189.340.503-12");
-			System.out.println("email-"+ c.getEmail()+"\nnome-"+c.getNome()+"\nnick -"+c.getNickname());
-		}catch(ClienteNaoCadastradoException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		if (arrayClientes.existe("192.949.340-53")) {
-			System.out.println("O cliente: 192.949.340-53 -- Existe");
-		}else {
-			System.out.println("O cliente: 192.949.340-53 -- Nao existe");
-		}
-		System.out.println("------------------");
-		
-		//lista
-		listaClientes.inserir(cl);
-		listaClientes.inserir(cl1);
-		System.out.println(listaClientes.existe("192.949.340-53"));
-		System.out.println(listaClientes.existe("189.340.503-12"));
-		
-		try {
-			listaClientes.atualizar(cl2);
-			listaClientes.remover("189.340.503-12");
-			listaClientes.remover("123");
-		}catch(ClienteNaoCadastradoException e) {
-			System.out.println(e.getMessage());
-		}
-			System.out.println(listaClientes.existe("189.340.503-12"));
-		try {
-			Cliente c = listaClientes.procurar("192.949.340-53");
-			if(c != null)
-			System.out.println("email-"+ c.getEmail()+"\nnome-"+c.getNome()+"\nnick -"+c.getNickname());
-		}catch(ClienteNaoCadastradoException e) {
-			System.out.println(e.getMessage());
-		}
-		//teste fachada
-		try {
-		f.cadastrarCliente(cl1);
-		}catch(ClienteJaCadastradoException e1) {
-			System.out.println(e1.getMessage());
+		String acao = "";
+		String classeEntrada="";
+		while(in.hasNext())
+		{
+			try
+			{
+				acao=in.nextLine();
+				classeEntrada=in.nextLine();
+				switch(acao)
+				{
+					case("CADASTRAR"):
+					{
+						switch(classeEntrada)
+						{
+							case("DESENVOLVEDOR"):
+								f1.cadastrarDesenvolvedor(new Desenvolvedor(in.nextLine(),in.nextLine(),in.nextLine(),Double.parseDouble(in.nextLine())));
+								//f1.cadastrarDesenvolvedor(new Desenvolvedor("Wiusu","wilsin@gamiu.com","400289-22",10.6));
+								break;
+						}
+						break;
+					}
+					case("REMOVER"):
+					{
+						switch(classeEntrada)
+						{
+							case("DESENVOLVEDOR"):
+							{
+								f1.removerDesenvolvedor(in.nextLine());
+								//f1.removerDesenvolvedor("400289-22");
+								break;
+							}
+						}
+						break;
+					}
+					case("PROCURAR"):
+					{
+						switch(classeEntrada)
+						{
+							case("DESENVOLVEDOR"):
+							{
+								f1.procurarDesenvolvedor(in.nextLine());
+								//f1.procurarDesenvolvedor("400289-22");
+								break;
+							}
+						}
+						break;
+					}
+					case("ATUALIZAR"):
+					{
+						switch(classeEntrada)
+						{
+							case("DESENVOLVEDOR"):
+							{
+								f1.atualizarDesenvolvedor(new Desenvolvedor(in.nextLine(),in.nextLine(),in.nextLine(),Double.parseDouble(in.nextLine())));
+								//f1.atualizarDesenvolvedor(new Desenvolvedor("Wiusu","wilsin@gamiu.com","400289-22",10.6));
+								break;
+							}
+						}
+						break;
+					}
+					case("EXISTE"):
+					{
+						switch(classeEntrada)
+						{
+							case("DESENVOLVEDOR"):
+							{
+								System.out.println(f1.existeDesenvolvedor(in.nextLine()));
+								//System.out.println(f1.existeDesenvolvedor("400289-22"));
+								break;
+							}
+						}
+						break;
+					}
+				}
+			}catch (DesenvolvedorJaCadastradoException e1) {
+				e1.printStackTrace();
+			}catch (DesenvolvedorNaoCadastradoException e2) {
+				e2.printStackTrace();
+			}//AQUI ENTRA OS CATCH'S COM OS EXCEPTION DOS OUTROS 
 		}
 		in.close();
 	}
 }
+
